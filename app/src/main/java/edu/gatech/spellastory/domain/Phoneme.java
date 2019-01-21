3a8fc08c13179de7c4b17f0cd25fd04f4468eb0a
Jake Cheek <jake@f-m.fm>
2019-01-21 10:19:16 -0500

    Create base word and phoneme classes

---
+++ app/src/main/java/edu/gatech/spellastory/domain/Phoneme.java
@@ -0,0 +1,9 @@
+package edu.gatech.spellastory.domain;
+
+public class Phoneme {
+
+    private String audio;
+    private String spelling;
+    private String color;
+
+}
---
+++ app/src/main/java/edu/gatech/spellastory/domain/Word.java
@@ -0,0 +1,11 @@
+package edu.gatech.spellastory.domain;
+
+import java.util.List;
+
+public class Word {
+
+    private List<Phoneme> spelling;
+    private String picture;
+    private boolean completed;
+
+}
