14e2e387c06c5e02470bbf25e6893dd425488dd2
Jake Cheek <jake@f-m.fm>
2019-01-21 18:16:33 -0500

    Create phoneme list activity

---
+++ app/src/main/java/edu/gatech/spellastory/PhonemeListActivity.java
@@ -0,0 +1,14 @@
+package edu.gatech.spellastory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhonemeListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phoneme_list);
+    }
+
+}
