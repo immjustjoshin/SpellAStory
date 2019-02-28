572993128e6aa15fd895b47bc65dbe9e5e9ab49b
Jake Cheek <jake@f-m.fm>
2019-02-28 10:32:47 -0500

    Remove unused imports

--- app/src/androidTest/java/edu/gatech/spellastory/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/edu/gatech/spellastory/ExampleInstrumentedTest.java
@@ -1,8 +1,6 @@
 package edu.gatech.spellastory;
 
 import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
