2fcbb8c460c60d8c41acdc2dfed239f9f3db35fb
chess7 <chess7@gatech.edu>
2019-04-03 22:13:50 -0400

    added code for getting random words by category

--- app/src/main/java/edu/gatech/spellastory/data/Words.java
+++ app/src/main/java/edu/gatech/spellastory/data/Words.java
@@ -1,6 +1,9 @@
 package edu.gatech.spellastory.data;
 
 
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+
 import com.opencsv.CSVReader;
 
 import java.io.FileNotFoundException;
@@ -8,12 +11,18 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Random;
 
-class Words {
+import com.google.gson.Gson;
+
+import edu.gatech.spellastory.domain.Word;
+
+class Words extends AppCompatActivity {
 
     // Maps word to phoneme codes
     // Ex. swan -> [95,3,61]
@@ -62,8 +71,51 @@ class Words {
         return new ArrayList<>(words.keySet());
     }
 
-    List<String> getWordsForCategory(String category) {
-        return categoryMap.get(category);
+    // return a list of **up to** k random solved words for a category, may not be k solved words available
+    List<String> getSolvedWordsFromCategory(int k, String category) {
+        List<Word> completed = getCompletedWords();
+        Collections.shuffle(completed); // randomize list
+
+        List<String> out = new ArrayList<>();
+
+        List<String> categoryWords = categoryMap.get(category);
+        if (categoryWords == null) return out;
+        int i = 0;
+        for (Word word : completed) {
+            if (i >= k) return out; // retrieve at most k words
+            String temp = word.getSpelling();
+            if (categoryWords.contains(temp)) {
+                out.add(temp);
+                i++;
+            }
+        }
+        return out;
+    }
+
+    // return a list of **up to** k random unsolved words for a category, may not be k unsolved words available
+    List<String> getUnsolvedWordsFromCategory(int k, String category) {
+        List<Word> completed = getCompletedWords();
+        List<String> out = new ArrayList<>();
+
+        List<String> completedString = new ArrayList<>();
+        for (Word temp : completed) {
+            completedString.add(temp.getSpelling());
+        }
+
+        List<String> categoryWords = categoryMap.get(category);
+        if (categoryWords == null) return out;
+        Collections.shuffle(categoryWords);
+
+        int i = 0;
+        for (String word : categoryWords) {
+            if (i >= k) return out; // retrieve at most "amount" words
+            if (!completedString.contains(word)) {
+                out.add(word);
+                i++;
+            }
+        }
+
+        return out;
     }
 
     boolean wordContains(String word, String phonemeCode) {
@@ -71,4 +123,21 @@ class Words {
                 && Objects.requireNonNull(words.get(word)).contains(phonemeCode);
     }
 
+    /* Took function from WordBankListActivity.java, probably don't need to copy everything over
+       but I just put getWordsFromCategory() in this class for the time being */
+    private List<Word> getCompletedWords() {
+        List<Word> wordsCompleted = new ArrayList<>();
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("completedWords", MODE_PRIVATE);
+        Map<String, ?> map = pref.getAll();
+        List<String> keys = new ArrayList<>(map.keySet());
+        Gson gson = new Gson();
+        for (int i = 0; i < keys.size(); i++) {
+            String json = pref.getString(keys.get(i), "");
+            Word word = gson.fromJson(json, Word.class);
+            if (word.isComplete()) {
+                wordsCompleted.add(word);
+            }
+        }
+        return wordsCompleted;
+    }
 }
